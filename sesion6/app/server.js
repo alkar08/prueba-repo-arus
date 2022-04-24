b549f594b577208211d674f0f2bc5c27e5555c8a
Alejandro Correa Rodriguez <alejandro.correa@arus.com.co>
2022-04-24 15:03:37 -0500

    Pruebas metodos post y get

--- sesion6/app/server.js
+++ sesion6/app/server.js
@@ -8,20 +8,20 @@ const app = express();
 // };
 
 let corsOptions = ['http://localhost:8081', 'http://localhost:8082'];
-app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin) return callback(null, true);
-            if (corsOptions.indexOf(origin) === -1){
-                return callback(
-                    new Error('La politica de CORS para esta url mo permite el acceso'),
-                    false
-                );
-            } return callback(null, true);
-        },
-        credentials: true
-    })
-)
+// app.use(
+//     cors({
+//         origin: (origin, callback) => {
+//             if (!origin) return callback(null, true);
+//             if (corsOptions.indexOf(origin) === -1){
+//                 return callback(
+//                     new Error('La politica de CORS para esta url mo permite el acceso'),
+//                     false
+//                 );
+//             } return callback(null, true);
+//         },
+//         credentials: true
+//     })
+// )
 
 app.use(cors(corsOptions));
 
@@ -29,17 +29,31 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded( {extends: true} ));
 
+
 app.get('/',(req, res) => {
-    res.json({ mensaje: 'Welcome!!! to my first API'});
+    const {id, name} = req.query; // desde el req.query se obtienen los parámetros enviados desde la url
+    console.log(id);
+    console.log(name);
+    res.json({ mensaje: 'Welcome!!! to my first API Method GET'});
 });
 
+app.post('/',(req, res) => {
+    console.log('-------------------------------------------------');
+    const {id, name} = req.body; // desde el req.body se obtiene lo que envía por el método post
+    console.log(id);
+    console.log(name);
+    // console.log(req.body);
+    // console.log(req.query);
+    console.log('-------------------------------------------------');
+    res.json({ mensaje: 'Welcome!!! to my first API Method POST'});
+});
 const PORT = 8081;
 
-app.listen( PORT, () => {
-    console.log(`Server corriendo por el puerto ${PORT}`);
-});
+// app.listen( PORT, () => {
+//     console.log(`Server corriendo por el puerto ${PORT}`);
+// });
 
 const server = app.listen(PORT, function() {
     let host = server.address().address;
-    console.log("SERVIDOR BACKEND EN htt´p;//%s:%s", host, PORT);
+    console.log("SERVIDOR BACKEND EN http;//%s:%s", host, PORT);
 });
